0e0f867508c739f9a2a6b38a9d87c7f9f3974838
f1lander <filanderuclez@gmail.com>
2016-07-17 13:47:34 -0600

    Incomplete DLR

--- js/app.js
+++ js/app.js
@@ -2,29 +2,31 @@ var app = angular.module('puvex',['pascalprecht.translate']);
 
 app.config(function ($translateProvider) {
   $translateProvider.translations('en', {
-    TITLE: 'Puvex - We code your ideas',
-    GREETINGS: 'Welcome To Our Studio!',
+    TITLE: 'Puvex',
+    GREETINGS: 'We code your ideas!',
     SERVICES: 'Services',
     PORTFOLIO: 'Portfolio',
-    ABOUT: 'About',
+    SKILLS: 'Our Skills',
     TEAM: 'Team',
     CONTACT: 'Contact',
-    WELCOME2: "It's Nice To Meet You",
-    TELLMEMORE: 'Tell Me More',
+    WELCOME2: "Let's Make Software",
+    TELLMEMORE: 'Know us',
+    WEBDEV: 'Web Development',
     INTROTEXT: 'Contact',
     BUTTON_LANG_EN: 'english',
     BUTTON_LANG_DE: 'spanish'
   });
   $translateProvider.translations('es', {
-    TITLE: 'Puvex - Codificamos sus ideas',
-    GREETINGS: '¡Bienvenido a Nuestro Estudio!',
+    TITLE: 'Puvex',
+    GREETINGS: '¡Codificamos sus ideas!',
     SERVICES: 'Servicios',
     PORTFOLIO: 'Portafolio',
+    SKILLS: 'Expertise',
     ABOUT: 'Acerca de',
     TEAM: 'Equipo',
     CONTACT: 'Contacto',
     WELCOME2: 'Es Un Placer Conocerle',
-    TELLMEMORE: 'Cu&eacute;ntenme m&aacute;s',
+    TELLMEMORE: 'Con&oacute;zcanos',
     INTROTEXT: 'Contacto',
     BUTTON_LANG_EN: 'ingl&eacute;s',
     BUTTON_LANG_DE: 'espa&ntilde;ol'
@@ -36,5 +38,6 @@ app.controller('MainCtrl', function ($scope, $translate) {
   $scope.changeLanguage = function (key) {
     $translate.use(key);
   };
+  $scope.Language = "Español";
 });
 
