93c1e320210e6a16812aef7cd67bb48a05452057
f1lander <filanderuclez@gmail.com>
2016-07-18 00:08:42 -0600

    Backup3

--- js/app.js
+++ js/app.js
@@ -1,42 +1,78 @@
 var app = angular.module('puvex',['pascalprecht.translate']);
 
 app.config(function ($translateProvider) {
-  $translateProvider.translations('en', {
-    TITLE: 'Puvex',
-    GREETINGS: 'We code your ideas!',
-    SERVICES: 'Services',
-    PORTFOLIO: 'Portfolio',
-    SKILLS: 'Our Skills',
-    ABOUT: 'Acerca de',
-    TEAM: 'Team',
-    CONTACT: 'Contact',
-    WELCOME2: "Let's Make Software",
-    TELLMEMORE: 'Know us',
+  $translateProvider.translations('en', {    
+    CHANGELANG: 'Cambiar a idioma Español',
     BUTTON_LANG_EN: 'Español',
-    SERVICESDESC: 'We are a small team of software engineers based in San José, Costa Rica. We have the mission to collaborate with our talent to projects anywhere in the world. We use an incremental development approach based on functional deliverables.',
+    HOME: 'Home',
+    TITLE: 'This is Puvex',
+    GREETINGS: 'We code your ideas!',
+    WELCOME2: "Let's Make Software",
+    TELLMEMORE: 'Know us',    
+    ABOUT: 'About',
+    ABOUT2: 'Who we are?',
+    SERVICES: 'Services',
+    SERVICES2: 'What do we do?',
+    TOOLS: 'Tools',
+    TOOLS2: 'How do we do it?',
+    PORTFOLIO: 'Portfolio',
+    TEAM: 'THE Team',
+    SERVICESDESC: 'Some of our services are:',
     WEBDEV: 'Web Development',    
     WEBDEVDESC: 'We make awesome complete websites and web applications with the latest technologies',
-    MOBILEDEV:'',
+    MOBILEDEV:'Mobile Development',
+    MOBILEDEVDESC:"Imagine an App... We'll make it possible for Android and iOS",
+    GAMESDEV:'Video Games',
+    GAMESDEVDESC:"Who doesn't like videogames? It's time to have fun coding creative software",  
+  
+    
+    CONTACT: 'Contact'
 
-  });
+});
+
+  /*
+   'We are a small team of software engineers 
+    based in San José, Costa Rica. 
+    We have the mission to collaborate with our talent to projects anywhere in the world. 
+    We use an incremental development approach based on functional deliverables.'
+  */
+
   $translateProvider.translations('es', {
-    TITLE: 'Puvex',
+    CHANGELANG: 'Switch to English Language',
+    BUTTON_LANG_EN: 'English',
+    HOME: 'Inicio',
+    TITLE: 'Somos Puvex',
     GREETINGS: '¡Codificamos sus ideas!',
-    SERVICES: 'Servicios',
-    PORTFOLIO: 'Portafolio',
-    SKILLS: 'Expertise',
-    ABOUT: 'Acerca de',
-    TEAM: 'Equipo',
-    CONTACT: 'Contacto',
     WELCOME2: 'Hagamos Software',
     TELLMEMORE: 'Conózcanos',
-    BUTTON_LANG_EN: 'English',
-    SERVICESDESC: 'Somos un pequeño equipo de ingenieros de software establecido en San José, Costa Rica.Tenemos la misión de aportar nuestro talento a proyectos por implementar en cualquier parte del Mundo. Entregamos los productos de manera incremental.',
+    ABOUT: '¿Quiénes somos?', 
+    ABOUT2: '¿Quiénes somos?', 
+    SERVICES: 'Servicios',
+    SERVICES2: '¿Qué hacemos?',
+    TOOLS: 'Herramientas',
+    TOOLS2: '¿Cómo lo hacemos?',
+    PORTFOLIO: 'Portafolio',
+    TEAM: 'EL Equipo',
+    SERVICESDESC: 'Algunos de nuestros servicios son:',
     WEBDEV: 'Desarrollo Web',    
     WEBDEVDESC: 'Hacemos portales Web Completos y Aplicaciones a la medida con tecnologías actuales',
-  });
+    MOBILEDEV:'Desarrollo para Móviles',
+    MOBILEDEVDESC:'El app que a usted se le ocurra, la hacemos posible para Android y iOS',
+    GAMESDEV:'Video Juegos',
+    GAMESDEVDESC:'¿A quién no le gustan los video juegos? Es tiempo de divertirse haciendo software creativo',
+  
+
+    CONTACT: 'Contacto'
+});
   $translateProvider.preferredLanguage('en');
 });
+
+/*
+  'Somos un pequeño equipo de ingenieros de software 
+   establecido en San José, Costa Rica.
+   Tenemos la misión de aportar nuestro talento a proyectos por implementar en cualquier parte del Mundo. 
+   Entregamos los productos funcionales de manera incremental.',
+*/
 
 app.controller('MainCtrl', function ($scope, $translate) {
   $scope.language = 'en';
