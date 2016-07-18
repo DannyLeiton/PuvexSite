d002654b9cfcbefe98ab090e0fa8c038bd531eb5
f1lander <filanderuclez@gmail.com>
2016-07-18 09:28:58 -0600

    Backup4

--- js/app.js
+++ js/app.js
@@ -10,7 +10,16 @@ app.config(function ($translateProvider) {
     WELCOME2: "Let's Make Software",
     TELLMEMORE: 'Know us',    
     ABOUT: 'About',
-    ABOUT2: 'Who we are?',
+    ABOUT2: 'Who are we?',
+    ABOUT3: 'A TEAM OF DEVELOPERS',
+    ABOUT4: 'Senior Engineers',
+    ABOUT5: 'We are a small team of software engineers based in San José, Costa Rica!',
+    ABOUT6: 'WITH GLOBAL VISION',
+    ABOUT7: 'Worldwide Collaboration',
+    ABOUT8: 'With the mission to collaborate with our talent to projects anywhere in the world',
+    ABOUT9: 'PRODUCT ORIENTED',
+    ABOUT10: 'Effective Development',
+    ABOUT11: 'Using an incremental development approach based on functional deliverables',
     SERVICES: 'Services',
     SERVICES2: 'What do we do?',
     TOOLS: 'Tools',
@@ -26,7 +35,8 @@ app.config(function ($translateProvider) {
     GAMESDEVDESC:"Who doesn't like videogames? It's time to have fun coding creative software",  
   
     
-    CONTACT: 'Contact'
+    CONTACT: 'Contact',
+    CONTACT2: 'Contact us'
 
 });
 
@@ -46,7 +56,16 @@ app.config(function ($translateProvider) {
     WELCOME2: 'Hagamos Software',
     TELLMEMORE: 'Conózcanos',
     ABOUT: '¿Quiénes somos?', 
-    ABOUT2: '¿Quiénes somos?', 
+    ABOUT2: '¿Quiénes somos?',
+    ABOUT3: 'UN EQUIPO DE PROGRAMADORES',
+    ABOUT4: 'Ingenieros Experimentados',
+    ABOUT5: 'Somos un pequeño equipo de ingenieros de software establecido en San José, Costa Rica!',
+    ABOUT6: 'CON VISION GLOBAL',
+    ABOUT7: 'Colaborando en todo el mundo',
+    ABOUT8: 'Tenemos la misión de aportar nuestro talento a proyectos por implementar en cualquier parte del Mundo',
+    ABOUT9: 'ORIENTADOS AL PRODUCTO',
+    ABOUT10: 'Desarrollo Efectivo',
+    ABOUT11: 'Entregamos los productos funcionales de manera incremental', 
     SERVICES: 'Servicios',
     SERVICES2: '¿Qué hacemos?',
     TOOLS: 'Herramientas',
@@ -62,7 +81,8 @@ app.config(function ($translateProvider) {
     GAMESDEVDESC:'¿A quién no le gustan los video juegos? Es tiempo de divertirse haciendo software creativo',
   
 
-    CONTACT: 'Contacto'
+    CONTACT: 'Contacto',
+    CONTACT2: 'Contáctenos'
 });
   $translateProvider.preferredLanguage('en');
 });
